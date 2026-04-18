e0795b6da37a5f82c7154d90bcfab2868496e319
k1mch1 <kimchourluy@gmail.com>
2026-04-19 02:02:44 +0700

    finished prototype of quiz logic

--- src/js/loadQuiz.js
+++ src/js/loadQuiz.js
@@ -25,27 +25,33 @@ async function prepareQuiz(path, selector){
   */
 
   let quizMain = document.querySelector(selector);
-  console.log(quizMain);
+
   let quizText = await fetch(path)
     .then(res => res.text());
+
   const parser = new DOMParser();
   let quizHtml = parser.parseFromString(quizText, 'text/html');
   // so DOMParser adds sematic tags wrapping like <head> <body> etc which is we need to work around
   let quizChildren = [...quizHtml.body.children];
   let quizHeading = quizChildren.shift();
+  document.querySelector('title').textContent = quizHeading.textContent;
   let quizInfo = quizChildren.shift();
   quizInfo.className = "quiz-card quiz-info";
   let quizQuestions = quizChildren;
+  
   quizQuestions.forEach((questionSection, i) => {
     questionSection.className = "quiz-card quiz-question";
+
+    // modification of the fieldset (shuffling and more)
     let oldFieldset = questionSection.querySelector('fieldset');
     let newFieldSet = oldFieldset.cloneNode(true);
     let newFieldSetChildren = [...newFieldSet.children];
     let legend = newFieldSetChildren.shift();
+
     newFieldSetChildren.forEach((multipleChoiceOption, j) => {
       let input = multipleChoiceOption.querySelector("input");
       input.setAttribute("type", "radio");
-      input.setAttribute("name", `q${i}`);
+      input.setAttribute("name", `q${i + 1}`);
       if (j == 0){
         // by convention, the 0th element is correct
         input.setAttribute("value", "true");
@@ -54,18 +60,24 @@ async function prepareQuiz(path, selector){
         input.setAttribute("value", "false");
       }
     })
+    shuffleArray(newFieldSetChildren);
+    oldFieldset.innerHTML = legend.outerHTML + newFieldSetChildren.map(el => el.outerHTML).join('');
 
-    oldFieldset.replaceWith(newFieldSet);
+    // add feedback div and button
+    let feedbackDiv = `<div class="feedback" id='fb${i + 1}'></div>`;
+    let submitButton = `<button class="question-submit" onclick="checkAnswer('q${i + 1}', 'fb${i + 1}')">Submit</button>`;
+    questionSection.innerHTML += feedbackDiv + submitButton;
   });
+
   shuffleArray(quizQuestions);
   //console.log(quizQuestions);
-  let quizQuestionsAsText = quizQuestions.map(section => section.outerHTML).join("");
+  let quizQuestionsAsText = quizQuestions.map((section, i) => {
+    const progressText = `<p class="progress">Question ${i+1} of ${quizQuestions.length}</p>`;
+    return progressText + section.outerHTML;
+  })
+  .join("");
+
   quizMain.innerHTML = quizHeading.outerHTML + quizInfo.outerHTML + quizQuestionsAsText;
-
-  /*
-  injectContent(`/quizzes/${quizName}.html`, ".quiz-main");
-  */
-
 }
 
 prepareQuiz(`/quizzes/${getQuizName()}.html`, ".quiz-main");
---
+++ src/js/quizLogic.js
@@ -0,0 +1,48 @@
+
+let correctAnswers = 0;
+let answeredQuestions = 0;
+
+function checkAnswer(questionName, feedbackId){
+  let totalQuestions = document.querySelectorAll(".quiz-question").length;
+  let selected = document.querySelector(`input[name="${questionName}"]:checked`);
+  let feedback = document.getElementById(feedbackId);
+
+  if (selected === null) {
+    feedback.textContent = 'Please select an answer first.';
+    return;
+  }
+  answeredQuestions++;
+
+  if (selected.value === "true"){
+    correctAnswers++;
+    feedback.textContent = "Correct!";
+    selected.closest("label").classList.add("success");
+  }
+  else {
+    feedback.textContent = "Incorrect!";
+    selected.closest("label").classList.add("error");
+    let correct = document.querySelector(`input[name="${questionName}"][value="true"]`)
+    correct.closest("label").classList.add("success");
+  }
+
+  // disable all radio button after submission
+  let possibleAnswers = document.querySelectorAll(`input[name="${questionName}"]`);
+  possibleAnswers.forEach(input => {
+    input.disabled = true;
+  });
+
+  // show the result if all questions have been answered
+  console.log("Answered question: ", answeredQuestions);
+  console.log("Total question: ", totalQuestions);
+  if (answeredQuestions === totalQuestions){
+    const quizResultSection = `
+    <section class="quiz-card quiz-result">
+        <h2>Quiz Complete!</h2>
+        <p>Your score: <strong id="score">${correctAnswers}</strong> / ${totalQuestions}</p>
+        <button onclick="location.reload()">Try Again</button>
+        <a href="/index.html">Home</a>
+    </section>
+    `
+    document.querySelector('.quiz-main').innerHTML += quizResultSection;
+  }
+}
