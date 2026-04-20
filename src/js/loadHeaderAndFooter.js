cb5ae2c330c7c7de06b57344eedf1ea86ed78272
k1mch1 <kimchourluy@gmail.com>
2026-04-20 22:13:07 +0700

    fixed theme preference

--- src/js/loadHeaderAndFooter.js
+++ src/js/loadHeaderAndFooter.js
@@ -17,14 +17,18 @@ injectContent(`${pathPrefix}/nav.html`, 'nav').then(() => {
   let theme = localStorage.getItem("theme");
   const preferDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const themeToggleButton = document.getElementById("theme-toggle");
-  if (preferDarkTheme){
-    localStorage.setItem("theme", "dark");
-    theme = "dark";
+
+  if (theme === null){
+    if (preferDarkTheme){
+      localStorage.setItem("theme", "dark");
+      theme = "dark";
+    }
+    else{
+      localStorage.setItem("theme", "light");
+      theme = "light";
+    }
   }
-  else{
-    localStorage.setItem("theme", "light");
-    theme = "light";
-  }
+
   themeToggleButton.innerHTML += toggleSwitchContent[theme];
   setTheme();
   themeToggleButton.addEventListener("click", () => {
